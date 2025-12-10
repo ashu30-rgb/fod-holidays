88d6b750b83ca8a8074a9827f2628ee5b7a517ab
Ashutosh <ashutosh728173@gmail.com>
2025-12-10 19:17:47 +0530

    added vercel speed insights

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import HomePage from "./home/page"
 
 export default function Home() {
   return (
     <div>
       <HomePage />
+      <SpeedInsights/>
     </div>
   );
 }
